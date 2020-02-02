428d3d1490c74540fc18117555dccd0a5e279425
Lee Chilvers <lee.chilvers@icloud.com>
2020-02-02 15:20:13 +0000

    commit

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-  var urls = ['indexBackground.jpeg', 'indexBackground5.jpeg', 'indexBackground4.jpeg', 'indexBackground7.jpeg', 'indexBackground2.jpeg', 'indexBackground3.jpeg', 'indexBackground6.jpeg', 'indexBackground9.jpeg', 'indexBackground8.jpeg'];
+// $(document).ready(function() {
+//   var urls = ['indexBackground.jpeg', 'indexBackground5.jpeg', 'indexBackground4.jpeg', 'indexBackground7.jpeg', 'indexBackground2.jpeg', 'indexBackground3.jpeg', 'indexBackground6.jpeg', 'indexBackground9.jpeg', 'indexBackground8.jpeg'];
 
-  var cout = 1;
-  $('body').css('background-image', 'url("' + urls[0] + '")');
-  setInterval(function() {
-    $('body').css('background-image', 'url("' + urls[cout] + '")');
-    cout == urls.length-1 ? cout = 0 : cout++;
-  }, 10000);
+//   var cout = 1;
+//   $('body').css('background-image', 'url("' + urls[0] + '")');
+//   setInterval(function() {
+//     $('body').css('background-image', 'url("' + urls[cout] + '")');
+//     cout == urls.length-1 ? cout = 0 : cout++;
+//   }, 10000);
 
-});
+// });
